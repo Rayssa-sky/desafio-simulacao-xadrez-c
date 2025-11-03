1b20f4a6de5403b9bfad1a71b5ed5d8227636af3
Rayssa-sky <rayssacortx@gmail.com>
2025-11-03 19:10:25 -0300

    Implement Knight movement logic in xadrez.c
    Added movement logic for the Knight piece in chess.

--- xadrez.c
+++ xadrez.c
@@ -5,7 +5,8 @@ int main(){
 int casas_Torre = 5;
 int casas_Bispo =5, contaBispo = 0;
 int casas_Rainha = 8, contaRainha = 0;
-int i;
+int passos_vertical;
+int passos_horizontal;
 
  printf("Movimento da Torre\n");
 
@@ -30,6 +31,24 @@ int i;
     contaRainha++;
 
   }while(contaRainha < casas_Rainha);
+
+ //CAVALO: Movimento de 2 casas para Baixo e 1 para a Esquerda. ESTRUTURA: Usando o loop 'for' //
+printf("Movimento do Cavalo\n");
+
+for (passos_vertical = 0; passos_vertical < 2; passos_vertical++) {
+        
+        //  Imprime o passo vertical
+        printf("Passo %d: Move para BAIXO\n", passos_vertical + 1);
+        passos_horizontal = 1 - passos_vertical; 
+        
+        // Loop WHILE Aninhado: 1 Passo Horizontal (para a esquerda)
+        while (passos_horizontal < 1) { 
+            printf("Passo 3: Move para ESQUERDA\n");
+            passos_horizontal++; 
+        }
+    }
+
+    printf("--- Movimento do Cavalo Completo em 'L' ---\n");
 
 
   return 0;
